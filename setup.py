b46d61e9a8ab8e5258670fa07a028388814dbc75
Derek Rodger <drodger@gmail.com>
2019-12-17 00:38:54 -0800

    Allow clear-lambda-storage to be installed via pip (#13)

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clear-lambda-storage",
+    version="1.0",
+    author="",
+    author_email="",
+    description="Clear Lambda code storage",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/epsagon/clear-lambda-storage",
+    py_modules=["clear_lambda_storage"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
