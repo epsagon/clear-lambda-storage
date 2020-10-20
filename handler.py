ab2914ca6a9041a27c5d88acc5861676cc5b4015
Víctor M. Mesas <victor.mesas@gmail.com>
2020-10-20 09:49:09 +0200

    add regions and profile when calling function

--- handler.py
+++ handler.py
@@ -3,5 +3,5 @@ from clear_lambda_storage import remove_old_lambda_versions
 
 
 def clear_lambda_storage(event, context):
-    remove_old_lambda_versions(Namespace(token_key_id=None, token_secret=None))
+    remove_old_lambda_versions(Namespace(token_key_id=None, token_secret=None, regions=None, profile=None))
     return "Successful clean! 🗑 ✅"
